fe91721f6750094651d3b53ebb684b266a96c1c6
Lucie Lénertová <lenertova.lucie@gmail.com>
2022-08-16 18:17:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# nespouštějte program!
+# jaké 2 řetězce se podle vás vytisknou do konzole? 
+# vaši předpověď napište do komentářů níže
+#
+#
+#
+
+weather = "rain"
+
+if weather == "sunny":
+    print("Take your sunglasses!")
+elif weather == "snow":
+    print("Take your coat!")
+elif weather == "rain":
+    print("Take your umbrella!")
+else:
+    print("No advice for you!")
+
+number = 6
+
+if number > 7:
+    print("Number is higher than seven")
+elif number < 7:
+    print("Number is lower than seven")
+else:
+    print("Both numbers are the same")
+
